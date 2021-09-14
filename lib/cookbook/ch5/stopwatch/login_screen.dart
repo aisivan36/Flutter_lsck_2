7be62e6999444f3cf492a9c3b933eb50c6a4e2a9
ivan-boginski <aisivan36@gmail.com>
2021-09-14 09:42:57 +0700

    flutter_dialogs

---
+++ lib/cookbook/ch5/stopwatch/login_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool loggedin = false;
+  String? name;
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  void _validate() {
+    final form = _formKey.currentState;
+    if (form != null) {
+      if (!form.validate()) {
+        return;
+      }
+    }
+
+    setState(() {
+      loggedin = true;
+      name = _nameController.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(
+        child: loggedin ? _buildSuccess() : _buildLoginForm(),
+      ),
+    );
+  }
+
+  Widget _buildSuccess() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.check,
+          color: Colors.orangeAccent,
+        ),
+        Text('Hi $name')
+      ],
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Runner'),
+              validator: (text) {
+                if (text != null) {
+                  // if (text.isEmpty) {
+                  //   return 'Enter the runner\'s name';
+                  // } else {
+                  //   return null;
+                  // }
+                  return text.isEmpty ? 'Enter the runner\'s name' : null;
+                }
+              },
+            ),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                //Check if it's not null
+                if (text != null) {
+                  if (text.isEmpty) {
+                    return 'Enter the runner\'s email';
+                  }
+                  final regex = RegExp('[^@]+@[^\.]+\..+');
+                  if (!regex.hasMatch(text)) {
+                    return 'Enter a valid email';
+                  }
+                }
+
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: _validate, child: Text('Continue')),
+          ],
+        ),
+      ),
+    );
+  }
+}
