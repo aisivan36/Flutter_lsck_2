c2de9b119d3ee188fd719c1397c4a00296a16caf
ivan-boginski <aisivan36@gmail.com>
2021-09-28 09:46:16 +0700

    GeoLocation Future

---
+++ lib/cookbook/Futures/geolocation.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({Key? key}) : super(key: key);
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  String myPosition = '';
+
+  @override
+  void initState() {
+    getPosition().then((Position myPos) {
+      myPosition = 'Latitude: ' +
+          myPos.latitude.toString() +
+          ' -Longtitude: ' +
+          myPos.longitude.toString();
+      setState(() {
+        myPosition = myPosition;
+      });
+    });
+    super.initState();
+  }
+
+  Future<Position> getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Current Location'),
+      ),
+      body: Center(
+        child: Text(myPosition),
+      ),
+    );
+  }
+}
